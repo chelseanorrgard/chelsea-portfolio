5918f9d592b2964cf124e9cb92bcde379df39eb2
Chelsea Norrgård <searenee@gmail.com>
2025-08-28 14:57:08 +0300

    Scroll-to-top functionality added when changing from one page to another

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import CV from './components/CV';
 import Hobbies from './components/Hobbies';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import ScrollToTop from './components/ScrollToTop';  // 👈 import it
 
 const App = () => {
   return (
     <Router>
       <div className="min-h-screen bg-pink-50">
         <NavBar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
